a654e0822f44215964021a8634dd3e896823b90a
metafates <vvz.dev@gmail.com>
2024-08-02 09:52:56 +0300

    change shiftwidth

--- init.lua
+++ init.lua
@@ -28,6 +28,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
