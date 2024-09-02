7f830a5b2df737157df3a11ef59ff8c69aba7c01
metafates <vvz.dev@gmail.com>
2024-09-02 09:40:27 +0300

    improvements

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -8,22 +8,24 @@ return {
 		"nvim-tree/nvim-web-devicons", -- optional dependency
 	},
 	opts = {
-		create_autocmd = true,
+		create_autocmd = false,
 	},
-	-- init = function()
-	-- 	vim.api.nvim_create_autocmd({
-	-- 		"WinScrolled", -- or WinResized on NVIM-v0.9 and higher
-	-- 		"BufWinEnter",
-	-- 		"CursorHold",
-	-- 		"InsertLeave",
-	--
-	-- 		-- include this if you have set `show_modified` to `true`
-	-- 		"BufModifiedSet",
-	-- 	}, {
-	-- 		group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-	-- 		callback = function()
-	-- 			require("barbecue.ui").update()
-	-- 		end,
-	-- 	})
-	-- end,
+	config = function(_, opts)
+		require("barbecue").setup(opts)
+
+		vim.api.nvim_create_autocmd({
+			"WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+			"BufWinEnter",
+			"CursorHold",
+			"InsertLeave",
+
+			-- include this if you have set `show_modified` to `true`
+			"BufModifiedSet",
+		}, {
+			group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+			callback = function()
+				require("barbecue.ui").update()
+			end,
+		})
+	end,
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,7 @@
 return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
+	cmd = "Mason",
 	dependencies = {
 		{
 			"williamboman/mason.nvim",
