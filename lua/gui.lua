4d109428dab2dfa866bd1002cc706f1ee72d4871
metafates <vvz.dev@gmail.com>
2024-09-10 09:20:41 +0300

    add gui settings

---
+++ lua/gui.lua
@@ -0,0 +1,52 @@
+local font = {
+	name = "JetBrainsMono Nerd Font Mono",
+	size = 16,
+}
+
+function font:sync()
+	vim.opt.guifont = { self.name, ":h" .. (self.current_size or self.size) }
+end
+
+function font:add_size(delta)
+	if self.current_size ~= nil then
+		self.current_size = self.current_size + delta
+	else
+		self.current_size = self.size + delta
+	end
+
+	self:sync()
+end
+
+function font:reset_size()
+	self.current_size = self.size
+
+	self:sync()
+end
+
+font:sync()
+
+local all_modes = { "n", "v", "s", "x", "o", "i", "l", "c", "t" }
+
+local map = vim.keymap.set
+
+map(all_modes, "<D-=>", function()
+	font:add_size(1)
+end)
+map(all_modes, "<D-->", function()
+	font:add_size(-1)
+end)
+map(all_modes, "<D-0>", function()
+	font:reset_size()
+end)
+
+local function paste_from_primary_clipboard()
+	vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+end
+
+map(all_modes, "<D-v>", paste_from_primary_clipboard, { noremap = true, silent = true })
+
+map(all_modes, "<D-}>", vim.cmd.bnext)
+
+map(all_modes, "<D-{>", vim.cmd.bprevious)
+
+map(all_modes, "<D-w>", vim.cmd.bd)
