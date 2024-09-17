b2094bd1b00a80bc87ad9f5b4b71fe7352b25a32
metafates <vvz.dev@gmail.com>
2024-09-17 17:18:17 +0300

    gui updates

--- lua/gui.lua
+++ lua/gui.lua
@@ -1,12 +1,18 @@
 local utils = require("utils")
 
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_input_macos_option_key_is_meta = "both"
+vim.g.neovide_cursor_smooth_blink = true
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,i-ci-ve-r-cr-o:blinkwait700-blinkoff400-blinkon175"
+vim.g.neovide_refresh_rate = 160
+
 local font = {
 	name = "JetBrainsMono Nerd Font Mono",
 	size = 16,
 }
 
 function font:sync()
-	vim.opt.guifont = { self.name, ":h" .. (self.current_size or self.size) }
+	vim.opt.guifont = { self.name, string.format(":h%d", self.current_size or self.size) }
 end
 
 function font:add_size(delta)
@@ -28,10 +34,10 @@ local all_modes = { "n", "v", "s", "x", "o", "i", "l", "c", "t" }
 local map = vim.keymap.set
 
 map(all_modes, "<D-=>", function()
-	font:add_size(1)
+	font:add_size(2)
 end)
 map(all_modes, "<D-->", function()
-	font:add_size(-1)
+	font:add_size(-2)
 end)
 map(all_modes, "<D-0>", function()
 	font:reset_size()
