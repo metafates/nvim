ff7629330b10ff99a6b538315fac0eb0a323f439
metafates <vvz.dev@gmail.com>
2025-03-31 16:56:46 +0300

    trim on write

--- lua/auto.lua
+++ lua/auto.lua
@@ -1,3 +1,10 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function() vim.hl.on_yank() end
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		MiniTrailspace.trim_last_lines()
+		MiniTrailspace.trim()
+	end
+})
