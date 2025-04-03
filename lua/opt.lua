9c5a8d702ad2bb1a162df7a202a80e6407b802c3
metafates <vvz.dev@gmail.com>
2025-04-03 10:40:56 +0300

    add options

--- lua/opt.lua
+++ lua/opt.lua
@@ -1,22 +1,27 @@
-vim.g.mapleader = " "
+vim.g.mapleader      = " "
 vim.g.maplocalleader = "\\"
 
-vim.opt.number = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.scrolloff = 5
-vim.opt.hlsearch = true
-vim.opt.autoread = true
-vim.opt.swapfile = false
-vim.opt.breakindent = true
+vim.opt.number       = true
+vim.opt.tabstop      = 4
+vim.opt.softtabstop  = 4
+vim.opt.shiftwidth   = 4
+vim.opt.scrolloff    = 5
+vim.opt.hlsearch     = true
+vim.opt.breakindent  = true
 
-vim.opt.undofile = true
+vim.opt.autoread     = true
+vim.opt.swapfile     = false
+vim.opt.undofile     = true
+vim.opt.backup       = false
+vim.opt.writebackup  = false
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.opt.ignorecase   = true -- Ignore case when searching (use `\C` to force not doing that)
+vim.opt.incsearch    = true -- Show search results while typing
+vim.opt.infercase    = true -- Infer letter cases for a richer built-in keyword completion
+vim.opt.smartcase    = true -- Don't ignore case when searching if pattern has upper case
+vim.opt.smartindent  = true -- Make indenting smart
 
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn   = 'yes'
 
 vim.opt.completeopt:append({ "fuzzy", "menuone", "preview", "noinsert" })
 
@@ -28,7 +33,9 @@ vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()' -- redefined with lsp on attach
 vim.opt.foldtext = ""
 vim.opt.foldcolumn = "0"
-vim.opt.fillchars:append({ fold = " " })
+vim.opt.fillchars:append({ eob = " ", fold = " " })
+
+vim.cmd.filetype("plugin indent on")
 
 -- vim.opt.winborder = 'shadow'
 vim.opt.winborder = 'single'
