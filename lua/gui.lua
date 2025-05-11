faf12a644a3135ca407684efa75305ebdb7e52fa
metafates <vvz.dev@gmail.com>
2025-05-11 13:03:01 +0300

    improve zen mode

--- lua/gui.lua
+++ lua/gui.lua
@@ -2,51 +2,16 @@ vim.g.neovide_remember_window_size = true
 vim.g.neovide_input_macos_option_key_is_meta = "both"
 vim.g.neovide_cursor_smooth_blink = true
 vim.g.neovide_refresh_rate = 160
-
-vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,i-ci-ve-r-cr-o:blinkwait700-blinkoff400-blinkon175"
-
 vim.g.neovide_opacity = 0.9
 vim.g.transparency = 0.9
 vim.g.neovide_window_blurred = true
+vim.g.neovide_show_border = true
 
--- local font = {
--- 	name = "JetBrainsMono Nerd Font Mono",
--- 	size = 16,
--- }
---
--- function font:sync()
--- 	vim.opt.guifont = { self.name, string.format(":h%d", self.current_size or self.size) }
--- end
---
--- function font:add_size(delta)
--- 	self.current_size = (self.current_size or self.size) + delta
---
--- 	self:sync()
--- end
---
--- function font:reset_size()
--- 	self.current_size = self.size
---
--- 	self:sync()
--- end
---
--- font:sync()
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,i-ci-ve-r-cr-o:blinkwait700-blinkoff400-blinkon175"
 
 local ALL_MODES = { "n", "v", "s", "x", "o", "i", "l", "c", "t" }
 
 local set = require("util.keymap").set
-
--- set(all_modes, "<D-=>", function()
--- 	font:add_size(2)
--- end)
---
--- set(all_modes, "<D-->", function()
--- 	font:add_size(-2)
--- end)
---
--- set(all_modes, "<D-0>", function()
--- 	font:reset_size()
--- end)
 
 set(ALL_MODES, "<D-v>", function()
 	vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
--- lua/util/zen.lua
+++ lua/util/zen.lua
@@ -1,31 +1,53 @@
 local M = {}
 
-local prev = {}
+local prev = {
+	o = {},
+	g = {},
+}
 
 local zen = {
-	nu = false,
-	rnu = false,
-	showtabline = 0,
-	cmdheight = 0,
-	signcolumn = "no",
-	laststatus = 0,
-	cursorline = false,
-	cursorcolumn = false,
-	foldcolumn = "0",
-	list = false,
-	ruler = false,
-	showcmd = false,
+	o = {
+		nu = false,
+		rnu = false,
+		showtabline = 0,
+		cmdheight = 0,
+		signcolumn = "no",
+		laststatus = 0,
+		cursorline = false,
+		cursorcolumn = false,
+		foldcolumn = "0",
+		list = false,
+		ruler = false,
+		showcmd = false,
+		linespace = 5,
+	},
+	g = {
+		neovide_scale_factor = 1.3,
+		neovide_padding_top = 50,
+		neovide_padding_bottom = 50,
+		neovide_padding_right = 50,
+		neovide_padding_left = 50,
+	},
 }
 
 function M.toggle()
 	if not vim.g.zen then
-		for name, value in pairs(zen) do
-			prev[name] = vim.o[name]
+		for name, value in pairs(zen.o) do
+			prev.o[name] = vim.o[name]
 			vim.o[name] = value
 		end
+
+		for name, value in pairs(zen.g) do
+			prev.g[name] = vim.g[name]
+			vim.g[name] = value
+		end
 	else
-		for name, value in pairs(prev) do
+		for name, value in pairs(prev.o) do
 			vim.o[name] = value
+		end
+
+		for name, value in pairs(prev.g) do
+			vim.g[name] = value
 		end
 	end
 
